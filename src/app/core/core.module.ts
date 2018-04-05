4f94c1d8e430066e65e45e8595d0f03a542224a1
el-davo <david.ahern26@gmail.com>
2018-04-05 21:22:32 +0100

    Added apollo

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LoadingComponent } from './loading/loading.component';
+import { Apollo, ApolloModule } from 'apollo-angular';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
   ],
   exports: [
     RouterModule,
@@ -18,4 +25,11 @@ import { LoadingComponent } from './loading/loading.component';
 })
 export class CoreModule {
 
+  constructor(apollo: Apollo,
+              httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({uri: '/graphql'}),
+      cache: new InMemoryCache()
+    });
+  }
 }
